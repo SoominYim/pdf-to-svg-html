d8ebc1c8b3a832286065dc09b46b82bdd381d37d
SoominYim <soo15soo@kakao.com>
2025-11-03 15:45:19 +0900

    refactor: Update vue.config.js 서버 엔드포인트 추가

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,21 +1,28 @@
 const { defineConfig } = require("@vue/cli-service");
+const target = process.env.TARGET || "http://localhost:3000";
+const devServer = {
+  proxy: {
+    "/upload": {
+      target: target,
+      changeOrigin: true,
+    },
+    "/convert": {
+      target: target,
+      changeOrigin: true,
+    },
+    "/getSVGFiles": {
+      target: target,
+      changeOrigin: true,
+    },
+    "/deleteFile": {
+      target: target,
+      changeOrigin: true,
+    },
+  },
+};
+
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: "../dist",
-  devServer: {
-    proxy: {
-      "/upload": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
-      "/convert": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
-      "/getSVGFiles": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
-    },
-  },
+  devServer: devServer,
 });
